879c593a4a3045d2e72ffb2f78c9f211e66dcbed
doaa <doaafakher0612@gmail.com>
2025-05-09 16:17:38 +0300

    Generate 10 random numbers

--- App.jsx
+++ App.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+  //     function generateAllNewDice(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+  function generateAllNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+     let random = Math.floor(Math.random() * 6);
+      newDice.push(random);
+    }
+    return newDice
+  }
+  console.log(generateAllNewDice());
+
   return (
     <main>
-   <div className="dice-container">
-      <Die value={1} />
-      <Die value={2} />
-      <Die value={3} />
-      <Die value={4} />
-      <Die value={5} />
-      <Die value={6} />
-      <Die value={7} />
-      <Die value={8} />
-      <Die value={9} />
-      <Die value={10} />
-   </div>
-    
+      <div className="dice-container">
+        <Die value={1} />
+       
+      </div>
     </main>
   );
 }
